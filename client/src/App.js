db11201eecbc39e858eecda78a0a87d4a335cf36
caleb <calebemorydye@gmail.com>
2023-05-09 21:30:44 -0600

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Result from './pages/Result'
+import About from './pages/About'
+import Message from './pages/Message'
+import Faqs from './pages/Faqs'
+import Appointment from './pages/Appointment'
+import ProjectOverview from './components/ProjectOverview'
+import Projects from './pages/Projects'
+import ChatModal from './components/ChatModal'
+import { AiOutlineMessage } from "react-icons/ai";
+import Redirect from './components/Redirect';
+import { Helmet } from "react-helmet";
+
+function App() {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Helmet>
+        <title>Only Drains</title>
+        <meta name="description" content="Only Drains offers expert plumbing services in Salt Lake City, Utah. Contact us today for a free quote!" />
+        <meta name="keywords" content="plumbing services, Salt Lake, Utah, Only Drains plumbing |have knowledgeable and experienced technicians, offer competitive pricing, and use high-quality materials. |Experience the Best Drain and Plumbing Services in Utah with Only Drains!" />
+        <meta name="author" content="Only Drains Plumbing" />
+        
+      </Helmet>
+       <div className="box-sizing:border-box bg-gray-100">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/message" element={<Message />} />
+        <Route path="/faqs" element={<Faqs />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/project-overview/:title" element={<ProjectOverview />} />
+        <Route path='/success' element={<Redirect />} />
+      </Routes>
+      <AiOutlineMessage
+        className="chat cursor-pointer  p-3 fixed
+          bottom-20 right-4 h-16 w-16 z-50 rounded-full"
+        onClick={() => setOpen(!open)}
+      />
+      {open && <ChatModal />}
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { WrenchScrewdriverIcon, EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/solid'
+import facebook from '../logo/facebook.jpg'
+import twitter from '../logo/twitter.png'
+import google from '../logo/google.jpg'
+import linkedin from '../logo/linkedin.png'
+
+const Navbar = () => {
+  return (
+    <header>
+      <div className="flex">
+        <div className="bg-blue-500 w-1/3 p-3 flex items-center px-5">
+          <span className="text-white font-bold text-[13px] md:text-[18px] company">Only Drains</span>
+        </div>
+        <div className="bg-paleBlue w-2/3 p-3 flex justify-end items-center md:pr-20">
+          {/* socials dont need for this site */}
+          {/* <div className='flex space-x-3'>
+          <a href="https://www.facebook.com/profile.php?id=100063464694068&mibextid=ZbWKwL"
+            target="_blank" rel="noreferrer" >
+            <img className="circle" src={facebook} alt="facebook logo" />
+          </a>
+          <a href="https://mobile.twitter.com/GasaliAbass" target="_blank" rel="noreferrer">
+            <img className="circle" src={twitter} alt="twitter logo" />
+          </a>
+          <a href=" https://www.instagram.com/invites/contact/?i=mi29glvkp6z6&utm_content=gwkaofn" target="_blank" rel="noreferrer">
+            <img className="circle" src={google} alt="google logo" />
+          </a>
+          <a href="https://www.linkedin.com/in/abass-kasali-369911247" target="_blank" rel="noreferrer">
+            <img className="circle" src={linkedin} alt="linkedin logo" />
+          </a>
+        </div> */}
+
+        </div>
+      </div>
+      <div className="flex justify-center md:flex items-center justify-around  py-2 px-3 md:px-10 bg-white">
+        <div className="md:flex md:flex-col items-baseline ">
+          <NavLink to="/" className="link flex items-center">
+          <WrenchScrewdriverIcon className="h-6 w-6 md:h-8 md:w-8 text-blue-500" />
+          <span className="md:text-2xl mx-2 text-blue-900 font-bold">Chandler Ostler</span>
+          </NavLink>
+          <p className="text-sm">Professional Plumber</p>
+        </div>
+
+          <div className="flex md:space-x-20 space-x-5">
+            <span className="border border-blue-400 p-2">
+                <PhoneIcon className="h-6 w-6 text-blue-400" />
+            </span>
+            <div className="flex flex-col justify-center items-baseline">
+                <p className="font-bold">Call us on</p>
+                <a href="tel:+2348147603570" className="text-sm grey">385-229-9779</a>
+            </div>
+          </div>
+
+        <div className="hidden md:flex space-x-3 items-center">
+          <div className="flex space-x-3 items-center">
+            <span className="border border-blue-400 p-2">
+                <EnvelopeIcon className="h-6 w-6 text-blue-400" />
+            </span>
+            <div className="flex flex-col justify-center items-baseline">
+              <p className="font-bold">Email us</p>
+              <a href="mailto:onlydrains@gmail.com" className="text-sm grey">OnlyDrains@gmail.com</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <nav className="flex justify-center  items-center
+        bg-paleBlue px-3  md:pr-0
+       text-white mx-auto text-[12px] md:text-[15px] font-bold">
+        <ul className="flex space-x-4 md:space-x-10  md:mx-3 py-6">
+        <NavLink className="link font-bold" to="/">HOME</NavLink>
+        <li><NavLink className="link font-bold" to="/about">ABOUT US</NavLink></li>
+        <li data-to-scrollspy-id="services"><NavLink className="link font-bold"  to="/result">SERVICES</NavLink></li>
+        <li><NavLink className="link font-bold " to="/projects">PROJECTS</NavLink></li>  
+        </ul>
+        {/* <li className="hidden md:block hover:opacity-90 
+        active:bg-blue-700 bg-blue-500 px-5 py-6 font-bold">BLOG</li> */}
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
